55a4527ececd0bc0f9f2ceff946bcfda077bb88c
gobbigustavo <gobbigustavo@me.com>
2015-09-30 19:02:35 -0300

    Dijkstra ajeitado + Queue
    Roda ae o queue no eclipse, nics vlw

---
+++ stringtoqueue-queue.c
@@ -0,0 +1,43 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+typedef struct pqueue PriorityQueue;
+PriorityQueue* createPriorityQueue();
+PriorityQueue* enqueue(PriorityQueue *pq, int i, int p);
+int dequeue(PriorityQueue *pq);
+int maximum(PriorityQueue *pq);
+int isEmpty(PriorityQueue *pq);
+void printPriorityQueue(PriorityQueue *pq);
+typedef struct Node Node;
+
+struct Node {
+char character;
+int priority;
+Node *nextNode;
+};
+
+
+
+struct pqueue {
+Node *first;
+};
+
+PriorityQueue* enqueue(PriorityQueue *pq, int i, int p) {
+    Node *newNode = (Node*) malloc(sizeof(Node));
+    newNode->i = i;
+    newNode->p = p;
+    if ((isEmpty(pq)) || (p > pq->first->p)) {
+        newNode->nextNode = pq->first;
+        pq->first = newNode;
+    }
+    else {
+        Node *current = pq->first;
+        while ((current->nextNode != NULL) && (current->nextNode->p > p)) {
+            current = current->nextNode;
+        }
+        newNode->nextNode = current->nextNode;
+        current->nextNode = newNode;
+    }
+    return pq;
+}
