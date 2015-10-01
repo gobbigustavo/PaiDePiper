16bf04424bd828a70dcebb888351ea5c66baa70e
gobbigustavo <gobbigustavo@me.com>
2015-10-01 16:29:41 -0300

    Substituição das variáveis i e p; criação do QueueisEmpty

--- stringtoqueue-queue.c
+++ stringtoqueue-queue.c
@@ -4,7 +4,7 @@
 
 typedef struct pqueue PriorityQueue;
 PriorityQueue* createPriorityQueue();
-PriorityQueue* enqueue(PriorityQueue *pq, int i, int p);
+PriorityQueue* enqueue(PriorityQueue *pq, int character, int priority);
 int dequeue(PriorityQueue *pq);
 int maximum(PriorityQueue *pq);
 int isEmpty(PriorityQueue *pq);
@@ -23,21 +23,25 @@ struct pqueue {
 Node *first;
 };
 
-PriorityQueue* enqueue(PriorityQueue *pq, int i, int p) {
+PriorityQueue* enqueue(PriorityQueue *pq, int character, int priority) {
     Node *newNode = (Node*) malloc(sizeof(Node));
-    newNode->i = i;
-    newNode->p = p;
-    if ((isEmpty(pq)) || (p > pq->first->p)) {
+    newNode->character = character;
+    newNode->priority = priority;
+    if ((isEmpty(pq)) || (priority > pq->first->priority)) {
         newNode->nextNode = pq->first;
         pq->first = newNode;
     }
     else {
         Node *current = pq->first;
-        while ((current->nextNode != NULL) && (current->nextNode->p > p)) {
+        while ((current->nextNode != NULL) && (current->nextNode->priority > priority)) {
             current = current->nextNode;
         }
         newNode->nextNode = current->nextNode;
         current->nextNode = newNode;
     }
     return pq;
+}
+
+QueueisEmpty(PriorityQueue *pq){
+return (pq->first == NULL);
 }
