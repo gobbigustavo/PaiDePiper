dd62027158ef65afc1bfb256557eadb423216bdd
Carla Nicole Calheiros Pimentel <carlanicole@ic.ufal.br>
2015-10-14 19:44:36 -0300

    bitThroughTree

--- encode_decode.c
+++ encode_decode.c
@@ -50,7 +50,7 @@ List *CreateEmptyList(){
     return;
 }
 
-struct Tree {
+struct Tree{
     int item;
     int visited = 0;
     struct Tree *leftNode;
@@ -122,38 +122,43 @@ encode(char uncompressed_text){
 
 ///--------------------------------------------------DECODE----------------------------------------------------------------------//
 
-
-    int getCodeSize(File* compressedFile){
-        File *file = fopen(compressedFile, "r");
-        fseek(file, 0, SEEK_END);
-        long int fileSize = ftell(file);
-        return fileSize;
-    }
-
-    int getCodeBitSize(long int fileSize, int trash){
-        long int bitFileSize = (fileSize - trash) * 8; ///quantidade de bits do arquivo sem o lixo
-        return bitFileSize;
-    }
-
-decode(File *compressedFile){
-    fopen(compressedFile); ///descobrir se precisa de alguma outra função pra ler o arquivo
-    getCodeSize(File *compressedFile); ///fazer função pra pegar o tamanho
-    File *file =
-
-
-}
-
-
-
-
-
-//------ READHEADER ---------//
 struct HeaderFiles{
     int trash;
+    long int fileSize;
+    //long int bitFileSize;
     unsigned char tree;
     unsigned int treeSize;
 };
 typedef struct HeaderFiles HeaderFiles;
+
+
+     void getCodeSize(File* compressedFile){
+        File *file = fopen(compressedFile, "r");
+        fseek(file, 0, SEEK_END);
+        long int fileSize = ftell(file);
+        fileSize = fileSize - HeaderFiles->treeSize - 2; ///take the 2 header bytes and the tree size
+        HeaderFiles->fileSize = fileSize;
+    }
+
+    /*void getCodeBitSize(HeaderFiles *HeaderFiles){
+        long int bitFileSize = (HeaderFiles->fileSize - HeaderFiles->trash) * 8; ///quantidade de bits do arquivo sem o lixo
+        HeaderFiles->bitFileSize = bitFileSize;
+    }*/
+
+//------ READHEADER ---------//
+
+    unsigned int getTrash(unsigned char byte1){
+        unsigned int trash;
+        trash = (int) byte1 >> 5;
+        return trash;
+    }
+
+    unsigned int getTreeSize(unsigned char byte1, unsigned char byte2){
+        unsigned int treeSize = byte1  & 31; ///set the first 3 bits 0
+        treeSize = treeSize << 8; ///coloca os bits já existentes 8 casas pra esquerda pois eles são o começo do tamanho e o fim é o byte 2
+        treeSize = treeSize | (unsigned int) byte2; ///coloca o byte 2 no fim do treeSize, juntando assim os bits do tamanho completo da arvore
+        return treeSize;
+    }
 
 
     void readHeader(File *compressedFile){
@@ -180,24 +185,9 @@ typedef struct HeaderFiles HeaderFiles;
     }
 
 
-    int getTrash(unsigned char byte1){
-        int trash;
-        trash = (int) byte1 >> 5;
-        return trash;
-    }
-
-    unsigned int getTreeSize(unsigned char byte1, unsigned char byte2){
-        unsigned int treeSize = byte1  & 31; ///set the first 3 bits 0
-        treeSize = treeSize << 8; ///coloca os bits já existentes 8 casas pra esquerda pois eles são o começo do tamanho e o fim é o byte 2
-        treeSize = treeSize | (unsigned int) byte2; ///coloca o byte 2 no fim do treeSize, juntando assim os bits do tamanho completo da arvore
-        return treeSize;
-    }
-
 //------END OF READHEADER ---------//
 
-
-
-    void rebuildTree(HeaderFiles *treeSize, HeaderFiles *tree){
+    void rebuildTree(HeaderFiles* HeaderFiles, unsigned char tree){
         int rt_index;
 
         Tree *rebuiltTree; ///criar nó vazio
@@ -262,7 +252,77 @@ typedef struct HeaderFiles HeaderFiles;
     }
 
 
+    void runFunction(File *compressedFile, HeaderFiles* HeaderFiles){
+        getCodeSize(compressedFile);
+        readHeader(compressedFile);
+        getCodeBitSize(HeaderFiles* HeaderFiles);
+        rebuildTree(HeaderFiles* HeaderFiles);
+    }
 
+
+decode(File *compressedFile){
+    fopen(compressedFile); ///descobrir se precisa de alguma outra função pra ler o arquivo
+    runFunction(compressedFile, HeaderFiles* HeaderFiles);
+
+
+
+}
+
+    bitThroughTree(Tree* rebuiltTree, File* compressedFile, HeaderFiles* HeaderFiles){
+        int index, bit_index;
+        unsigned char bytes = malloc(HeaderFiles->fileSize * (sizeof(unsigned char)));
+        unsigned char current_bit;
+
+        for(index = 0; index < HeaderFiles->fileSize; index++){
+            fread(&bytes[index], 1, 1, compressedFile); ///li 1 byte
+            if(index != (HeaderFiles->fileSize - 1)){ ///se não é o último byte
+                for(bit_index = 7; bit_index >= 0; bit_index --){
+                    current_bit = bytes[index] >> bit_index;
+                    current_bit = current_bit & 1;
+                    putBitOnTree(current_bit);
+                }
+            }
+            else{///se for o ultimo byte subtrai o lixo
+                for(bit_index = 7; bit_index > (7 - HeaderFiles->trash); bit_index--){
+                    current_bit = bytes[index] >> bit_index;
+                    current_bit = current_bit & 1;
+                    putBitOnTree(current_bit)
+
+                }
+            }
+        }
+    }
+
+
+
+
+     ///   FALTA :  PUTBITONTREE
+     ///            FILETREETOARRAY
+
+
+
+
+
+
+
+/*      todos os bits
+            11010101 >> 7      00000001   &1    00000001 -> 1
+            11010101 >> 6      00000011   &1    00000001 -> 1
+            11010101 >> 5      00000110   &1    00000000 -> 0
+            11010101 >> 4      00001101   &1    00000001 -> 1
+            11010101 >> 3      00011010   &1    00000000 -> 0
+            11010101 >> 2      00110101   &1    00000001 -> 1
+            11010101 >> 1      01101010   &1    00000000 -> 0
+            11010101 >> 0      11010101   &1    00000001 -> 1
+        ultimo bit
+
+            00000011 >> 2 00000000  &1  00000000
+            00000011 >> 1 00000001  &1  00000001
+            00000011 >> 0 00000011  &1  00000001
+
+
+
+*/
 
 
 ///--------------------------------------------------DECODE----------------------------------------------------------------------///
